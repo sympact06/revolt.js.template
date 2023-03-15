3c87cfd626b2af63c9bf7f505a33989de410dbe2
Olivier <47295195+sympact06@users.noreply.github.com>
2023-03-15 21:46:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "GFwuu1J-WfuWwC3VoD9FRHlAT-pQ7tqx4iL8LnDwjtcMLSvO7mnElNi4gtRnrhs1", // Put your bot token here
+    token: "TOKEN", // Put your bot token here
     prefix: "!", // Put your bot prefix here
   };
   
