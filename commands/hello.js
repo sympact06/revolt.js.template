1f227bc8135037e4a896ce516e2fdf921a7543a9
Olivier <47295195+sympact06@users.noreply.github.com>
2023-04-07 23:48:01 +0200

    Updated Template V0.1.2
    Added logging/handlers/better embed

--- commands/hello.js
+++ commands/hello.js
@@ -3,6 +3,7 @@ const SympactEmbedBuilder = require("../lib/embedBuilder");
 module.exports = {
   name: "hello",
   description: "Say hello!",
+  category: "Example Category!",
   async execute(message, args) {
     const embed = new SympactEmbedBuilder()
       .setTitle("Hello, world!")
--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-"use strict";
-
 const { Client } = require("revolt.js");
 const fs = require("fs");
-const { token, prefix } = require("./config.js");
+const { token, prefix, logging } = require("./config.js");
+const { Logger } = require("./lib/Logging");
+const { Message } = require("./lib/Message");
+const message = new Message("0.1.2");
+const SympactEmbedBuilder = require("./lib/EmbedBuilder");
+
+class Command {
+  constructor(name, description, execute) {
+    this.name = name;
+    this.description = description;
+    this.execute = execute;
+  }
+}
+
 class MyRevoltBot {
   constructor() {
+    this.logger = new Logger(logging);
     this.client = new Client();
     this.commands = new Map();
     this.setupListeners();
     this.loadCommands();
   }
+
   setupListeners() {
     this.client.on("ready", () =>
-      console.info(`Logged in as ${this.client.user.username}!`)
+      this.logger.info(
+        `Logged in as ${this.client.user.username}!`,
+        "MyRevoltBot"
+      )
     );
     this.client.on("message", this.handleMessage.bind(this));
+    message.start();
   }
+
   async handleMessage(message) {
     try {
       if (
@@ -25,44 +43,91 @@ class MyRevoltBot {
         message.author.bot
       )
         return;
+
       const args = message.content.slice(prefix.length).trim().split(/ +/);
       const commandName = args.shift().toLowerCase();
       const command = this.commands.get(commandName);
-      if (!command) return;
+      if (!command) {
+        this.logger.warn(
+          `Command ${commandName} executed but not found in the commands folder.`,
+          "MyRevoltBot"
+        );
+        return;
+      }
       command.execute(message, args);
+      this.logger.info(`Command ${commandName} executed.`, "MyRevoltBot");
     } catch (err) {
-      console.error("Error handling message:", err);
+      this.logger.error("Error handling message:", err, "MyRevoltBot");
     }
   }
+
   loadCommands() {
     const commandFiles = fs
       .readdirSync("./commands")
       .filter((file) => file.endsWith(".js"));
+
     for (const file of commandFiles) {
-      const command = require(`./commands/${file}`);
-      this.commands.set(command.name, command);
+      try {
+        const { name, description, execute } = require(`./commands/${file}`);
+        const command = new Command(name, description, execute);
+        this.commands.set(name, command);
+      } catch (err) {
+        this.logger.error(
+          `Error loading command file '${file}':`,
+          err,
+          "MyRevoltBot"
+        );
+      }
     }
   }
+
   async login() {
     try {
       await this.client.loginBot(token);
     } catch (err) {
-      console.error("Error logging in:", err);
+      this.logger.error("Error logging in:", err, "MyRevoltBot");
     }
   }
+
   async help(message) {
-    const commandList = Array.from(this.commands.values())
-      .map((command) => `\`${prefix}${command.name}\`: ${command.description}`)
-      .join("\n");
-    await message.channel.sendMessage(`Available commands:\n${commandList}`);
+    const commandList = Array.from(this.commands.values()).reduce(
+      (categories, command) => {
+        const category = command.category || "General";
+        if (!categories[category]) {
+          categories[category] = [];
+        }
+        categories[category].push(
+          `\`${prefix}${command.name}\`: ${command.description}`
+        );
+        return categories;
+      },
+      {}
+    );
+    const embedBuilder = new SympactEmbedBuilder()
+      .setTitle("Available Commands")
+      .setColor("#0099ff");
+    for (const [category, commands] of Object.entries(commandList)) {
+      const categoryString = `**${category}**`;
+      const commandsString = commands.join("\n");
+      embedBuilder.setDescription(
+        `${
+          embedBuilder.embed.description
+            ? `${embedBuilder.embed.description}\n\n`
+            : ""
+        }${categoryString}\n${commandsString}`
+      );
+    }
+    await message.reply({ embeds: [embedBuilder.build()] });
   }
 }
+
 const myBot = new MyRevoltBot();
 myBot.login();
-myBot.commands.set("help", {
-  name: "help",
-  description: "Displays a list of available commands.",
-  execute: myBot.help.bind(myBot),
-});
-
-// Revolt Template by @Sympact06 // index.js
+myBot.commands.set(
+  "help",
+  new Command(
+    "help",
+    "Displays a list of available commands.",
+    myBot.help.bind(myBot)
+  )
+);
